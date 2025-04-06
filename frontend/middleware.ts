0dfe5a41a29abe7172a7588611c5b087bb6905b5
Aprameya Kannan <101409453+aprameyak@users.noreply.github.com>
2025-04-06 01:05:42 -0400

    Editted middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,11 +1,16 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
- 
-export default clerkMiddleware();
- 
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse, NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const { userId } = getAuth(req);
+  if (!userId) {
+    const signInUrl = new URL("/sign-in", req.url);
+    signInUrl.searchParams.set("redirect_url", req.url);
+    return NextResponse.redirect(signInUrl);
+  }
+  return NextResponse.next();
+}
+
 export const config = {
-  matcher: [
-    "/((?!.*\\..*|_next).*)",
-    "/", 
-    "/(api|trpc)(.*)"
-  ],
+  matcher: "/((?!_next/static|_next/image|favicon.ico).*)",
 };
